c4e180e266cf4bbd5302ec0b01de8d066a72457c
Hirofumi Tanigami <tanigami@gmail.com>
2018-11-06 01:25:18 +0900

    start

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -40,23 +40,23 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     {
         $this->app->singleton(DestinationRegistry::class, function () {
             $destinationRegistry = new DestinationRegistry;
-            $destinationRegistry->setAll(config('notification.destinations'));
+            $destinationRegistry->setAll($this->app->make('config')->get('notification.destinations'));
             return $destinationRegistry;
         });
 
         $this->app->singleton(GatewayRegistry::class, function () {
             $gatewayRegistry = new GatewayRegistry;
-            $gatewayRegistry->setAll(config('notification.gateways'));
+            $gatewayRegistry->setAll($this->app->make('config')->get('notification.gateways'));
             return $gatewayRegistry;
         });
 
         $this->app->singleton(TemplateNotificationFactory::class, function () {
-            return new TemplateNotificationFactory(config('notification.template'));
+            return new TemplateNotificationFactory($this->app->make('config')->get('notification.template'));
         });
 
         $this->app->singleton(SendNotification::class, function () {
             $gatewayRegistry = new GatewayRegistry;
-            $gatewayRegistry->setAll(config('notification.gateways'));
+            $gatewayRegistry->setAll($this->app->make('config')->get('notification.gateways'));
             return new SendNotification($gatewayRegistry);
         });
 
