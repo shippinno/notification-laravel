30734dbed43052579d29ff822743d07a91cf6f14
kimurasayo <kimura34@hotmail.com>
2023-09-19 15:43:19 +0900

    symfony-mailerを使用する

--- config/notification.php
+++ config/notification.php
@@ -3,39 +3,34 @@
 use League\Flysystem\Adapter\Local;
 use League\Flysystem\Filesystem;
 use Maknz\Slack\Client;
-use Shippinno\Email\SwiftMailer\SwiftMailerSendEmail;
+use Shippinno\Email\SymfonyMailer\SymfonyMailerSendEmail;
 use Shippinno\Notification\Domain\Model\EmailDestination;
 use Shippinno\Notification\Domain\Model\SlackChannelDestination;
 use Shippinno\Notification\Infrastructure\Domain\Model\EmailGateway;
 use Shippinno\Notification\Infrastructure\Domain\Model\SlackGateway;
 use Shippinno\Template\Liquid;
 use Tanigami\ValueObjects\Web\EmailAddress;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mailer\Transport\Smtp\SmtpTransport;
 
 return [
     'destinations' => [
         // DestinationRegistry entries
     ],
-    'gateways' => [],
-//    'gateways' => [
-//        // GatewayRegistry entries
-//        'EmailDestination' => new EmailGateway(
-//            new SwiftMailerSendEmail(
-//                new Swift_Mailer(
-//                    (new Swift_SmtpTransport(
-//                        env('MAIL_HOST', 'example.com'),
-//                        env('MAIL_PORT', 25),
-//                        env('MAIL_ENCRYPTION', null)))
-//                        ->setUsername(env('MAIL_USERNAME', 'username'))
-//                        ->setPassword(env('MAIL_PASSWORD', 'password'))
-//                ),
-//                false
-//            ),
-//            new EmailAddress(env('NOTIFICATION_EMAIL_FROM', 'from@example.com'))
-//        ),
-//        'SlackChannelDestination' => new SlackGateway(
-//            new Client(env('NOTIFICATION_SLACK_WEBHOOK_URL', 'https://example.com'))
-//        ),
-//    ],
+    'gateways' => [
+        // GatewayRegistry entries
+        'EmailDestination' => new EmailGateway(
+            new SymfonyMailerSendEmail(
+                new Mailer(
+                    (new SmtpTransport(null, null, null)),
+                    null,
+                    null
+                ),
+        ), new EmailAddress(env('NOTIFICATION_EMAIL_FROM', 'from@example.com'))),
+        'SlackChannelDestination' => new SlackGateway(
+            new Client(env('NOTIFICATION_SLACK_WEBHOOK_URL', 'https://example.com'))
+        ),
+    ],
     // バージョンアップさせるために一旦コメントアウトしてエラー回避
     // 'template' => new Liquid(
     //     new Filesystem(
